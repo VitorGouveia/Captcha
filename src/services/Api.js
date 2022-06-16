392cafb3be111f80014d599001ee031a976848f5
Vitor Gouveia <vitorneves.gouveia10@gmail.com>
2022-06-16 19:06:14 -0300

    init yarn workspaces + color palette tool

--- src/services/Api.js
+++
@@ -1,9 +0,0 @@
-//axios é um client http que faz as chamdas e os requests ao back-end
-import axios from "axios"
-
-const Api = axios.create({
-    //url base usada em todos os requests do back-end
-    baseURL: process.env.REACT_APP_API_URL
-})
-
-export default Api
